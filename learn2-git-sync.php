859f0b5514590b99826d80b4032a953db1d01c1a
Paul Hibbitts <paul_hibbitts@sfu.ca>
2018-08-20 10:42:30 -0700

    Added configuration for default taxonomy category in theme setting.

--- learn2-git-sync.php
+++ learn2-git-sync.php
@@ -1,9 +1,17 @@
 <?php
 namespace Grav\Theme;
 
+use Grav\Common\Grav;
 use Grav\Common\Theme;
 
 class Learn2GitSync extends Learn2
 {
+
+  public static function getdefaulttaxonomycategory()
+  {
+    $config = Grav::instance()['config'];
+    return $config->get('themes.' . $config->get('system.pages.theme'). '.default_taxonomy_category');
+  }
+
 }
 ?>
