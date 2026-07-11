4218c60bbb6bc573b2693e73c8fa3e5c03158fed
Paul Hibbitts 🍁 <paul@paulhibbitts.com>
2026-07-11 06:23:24 -0700

    Fix Grav 2 tagfilter escaping by moving the H5P resizer script from inline content to the Assets API

--- shortcodes/H5PShortcode.php
+++ shortcodes/H5PShortcode.php
@@ -16,19 +16,14 @@ class H5PShortcode extends Shortcode
 
             $h5purl= $sc->getParameter('url', $sc->getBbCode());
 
-            if ($h5purl) {
-                $output = '<p><iframe src="'.$h5purl.'" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script></p>';
+            $h5psrc = $h5purl ?: $str;
+
+            if ($h5psrc) {
+                $this->grav['assets']->addJs('https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js');
+
+                $output = '<p><iframe src="'.$h5psrc.'" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>';
 
                 return $output;
-
-            } else {
-
-                if ($str) {
-                    $output = '<p><iframe src="'.$str.'" width="400" height="300" frameborder="0" allowfullscreen="allowfullscreen"></iframe><script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script></p>';
-
-                    return $output;
-                }
-
             }
 
         });
